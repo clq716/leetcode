9db02a613e1c817051bd5269a2ff0cbfc56757bc
Re_deer <clq716@gmail.com>
2026-06-24 10:32:39 +0800

    labuladong plugin data

---
+++ src/main/java/leetcode/labuladong/3700.锯齿形数组的总数 II.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=3700 lang=java
+ * @lcpr version=30403
+ *
+ * [3700] 锯齿形数组的总数 II
+ */
+
+// @lc code=start
+class Solution {
+    public int zigZagArrays(int n, int l, int r) {
+        
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 3\n4\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n1\n3\n
+// @lcpr case=end
+
+ */
+
